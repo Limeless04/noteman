7eaec59b98a24c20c45e81d75844ef69f63734c2
Limeless04 <alimvanjava@gmail.com>
2024-07-28 13:15:59 +0700

    noteman-pwa initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export'
+}
+export default withPWA(nextConfig);
